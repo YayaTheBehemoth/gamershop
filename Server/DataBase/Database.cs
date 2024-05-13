24655c5851c31376d5f1971b13777563e8bd27f8
YayaTheBehemoth <allanermansur@gmail.com>
2024-05-13 13:17:08 +0200

    modelklasser og npgsql + dapper setup

---
+++ Server/DataBase/Database.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+
+namespace gamershop.Server.Database;
+
+public class DbConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public DbConnectionFactory(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("ConnectionString");
+        
+        if (_connectionString == null)
+        {
+            throw new InvalidOperationException("Database connection string is missing.");
+        }
+    }
+
+    public NpgsqlConnection CreateConnection()
+    {
+        try
+        {
+            var connection = new NpgsqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error creating database connection: {ex.Message}");
+            throw;
+        }
+    }
+}
