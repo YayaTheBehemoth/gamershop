8b72a6ca6b597642ce16707bda924e3a8e96aacf
Scrumlord123 <mikkel@cargo-bi.com>
2024-05-13 14:24:38 +0200

    Controller

---
+++ Server/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gamershop.Server.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
