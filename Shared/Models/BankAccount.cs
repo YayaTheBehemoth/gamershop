c63c28d824ee1ff9cc60fc871b38aa561534785d
YayaTheBehemoth <allanermansur@gmail.com>
2024-05-19 12:53:13 +0200

    orders

---
+++ Shared/Models/BankAccount.cs
@@ -0,0 +1,10 @@
+namespace gamershop.Shared.Models
+{
+    public class BankAccount
+    {
+        public int AccountId { get; set; }
+        public string AccountHolderName { get; set; }
+        public string AccountNumber { get; set; }
+        public double Balance { get; set; }
+    }
+}
